cc67b397c266d119ad6645b34f568627cfc5254d
jayanth1234567 <84142658+jayanth1234567@users.noreply.github.com>
2023-08-19 10:33:51 +0530

    Add files via upload

---
+++ 5.calculator/src/JourneyCalculator.java
@@ -0,0 +1,13 @@
+
+public class JourneyCalculator {
+
+	public static void main(String[] args) {
+		JourneyCalculator journeyCalculator=new Jour
+		
+	}
+	public double CalculateDistance(double speed,double time) {
+		return 0;
+		
+	}
+
+}
